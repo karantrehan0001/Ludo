8052ae1d365011b1bfd4286bde0c26e2d4446e58
Karanveer <singhkv42@gmail.com>
2021-09-03 22:18:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+ludogame = () => {
+  const play1 = Math.floor(Math.random() * 6) + 1;
+  const play1dice = `images/dice${play1}.jpg`;
+  document.getElementById("check1").setAttribute("src", play1dice);
+  const play2 = Math.floor(Math.random() * 6) + 1;
+  const play2dice = `images/dice${play2}.jpg`;
+  document.getElementById("check2").setAttribute("src", play2dice);
+  if (play1 > play2) {
+    document.querySelector("h1").innerHTML = "Player1 won :)";
+  } else if (play1 < play2) {
+    document.querySelector("h1").innerHTML = "Player2 won :)";
+  } else {
+    document.querySelector('h1').innerHTML ="Draw"
+  }
+};
